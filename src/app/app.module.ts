60ecd6ccc956082d252ce52a9aefee72c51211aa
alok singh <aloks2573@gmail.com>
2018-04-05 16:21:31 +0530

    basics sevices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import {DishService} from './services/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +25,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule
     
   ],
-  providers: [],
+  providers: [DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
