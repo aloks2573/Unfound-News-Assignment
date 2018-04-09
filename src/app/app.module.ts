139b475c5fa9bf240ec583270dfba50cf10c60b5
alok singh <aloks2573@gmail.com>
2018-04-09 18:23:18 +0530

    Template driven form part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,11 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { FormsModule } from '@angular/forms';
 import {DishService} from './services/dish.service';
 import { PromotionService} from './services/promotion.service';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -30,14 +31,19 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
+  entryComponents: [
+    LoginComponent
+],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
     
   ],
   providers: [DishService,PromotionService, LeaderService ],
